e1e7153750ca498a95700235889fba191fdbf8c3
KassyAngel <kcevent37@gmail.com>
2025-10-31 10:16:26 +0000

    Fix API calls with Render URL for mobile

--- android/app/src/main/java/com/cartomystik/app/MainActivity.java
+++ android/app/src/main/java/com/cartomystik/app/MainActivity.java
@@ -4,16 +4,14 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
-import app.xplatform.capacitor.plugins.AdMob; // ✅ importe ton plugin AdMob
+
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // ✅ Enregistre le plugin AdMob au démarrage
-        registerPlugin(AdMob.class);
+       
     }
 
     @Override
